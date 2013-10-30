822dc5f5b0201dd8c877208b2a3cf105354c4bc5
Dinesh Weerapurage <xydinesh@gmail.com>
2013-10-30 15:32:48 -0400

    adding nav.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -134,3 +134,23 @@
 
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;;;; nav ;;;;
+(require 'nav)
+(nav-disable-overeager-window-splitting)
+
+;;;;;;;;;; Windows ;;;;;;;;;;;;;;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 78 :width normal)))))
+(add-to-list 'auto-mode-alist '("\\.cs$" . c++-mode))

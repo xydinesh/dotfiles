4228a24ea10a0232e04c00d9e5e26cbe5707dedb
Dinesh Weerapurage <xydinesh@gmail.com>
2013-10-31 12:48:14 -0400

    add ignore file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -147,6 +147,17 @@
       '("~/.emacs.d/snippets"            ;; personal snippets
         "~/.emacs.d/yasnippet/snippets"    ;; the default collection
         ))
+
+;;; using auto-complete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;;; set the trigger key so that it can work together with yasnippet on tab key,
+;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
+
 ;;;;;;;;;; Windows ;;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.

4ec2df7fcb614283bf439d14050b7d3b3ffd1a1f
Dinesh Weerapurage <xydinesh@gmail.com>
2013-11-01 13:44:08 -0400

    adding markdown mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -147,6 +147,14 @@
       '("~/.emacs.d/snippets"            ;; personal snippets
         "~/.emacs.d/yasnippet/snippets"    ;; the default collection
         ))
+
+;;; Markdown mode 
+(autoload 'markdown-mode "markdown-mode"
+        "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.txt\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 ;;;;;;;;;; Windows ;;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.

262676f570734559f6851b671d44cce4efec0146
Dinesh Weerapurage <dinesh.weerapurage@pearson.com>
2016-08-17 22:43:52 -0600

    adding new changes for init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;; -*- mode: emacs-lisp -*-
+;; -*- Mode: emacs-lisp -*-
 ;; Simple .emacs configuration
 
 ;; ---------------------
@@ -17,7 +17,6 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-;;(require 'cl)
 (require 'ido)
 (require 'ffap)
 (require 'uniquify)
@@ -80,6 +79,7 @@
 ;;kbd macro
 (global-set-key "\M-g" 'goto-line)
 (global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-q" 'set-120-columns)
 (global-set-key "\M-?" 'help-command)
 (global-set-key "\C-?" 'help-command)
 (global-set-key "\C-h" 'delete-backward-char)
@@ -92,20 +92,19 @@
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
 
-(global-set-key  [f2] 'server-edit)
-(global-set-key  [f3]  'call-last-kbd-macro)
-(global-set-key  [f4]  'ds)
+(global-set-key  [f2] 'pep8)
+(global-set-key  [f3]  'find-alternate-file)
+(global-set-key  [f4]  'set-80-columns)
 
 (global-set-key  [f5]  'enlarge-window)
 (global-set-key  [f6]  'shrink-window)
 (global-set-key  [f7]  'enlarge-window-horizontally)
 (global-set-key  [f8]  'shrink-window-horizontally)
 
-(global-set-key  [f9]  'compile)
+(global-set-key  [f9]  'prev-window)
 (global-set-key  [f10]  'other-window)
-(global-set-key  [f11]  'pep8)
+(global-set-key  [f11]  'compile)
 (global-set-key  [f12]  'set-mark-command)
-
 
 (defun line-to-top ()
   "current line to top of the window"
@@ -122,6 +121,24 @@
   (interactive)
   (scroll-down 1))
 
+(defun set-window-width (n)
+  "Set the selected window's width."
+  (adjust-window-trailing-edge (selected-window) (- n (window-width)) t))
+
+(defun set-80-columns ()
+  "Set the selected window to 80 columns."
+  (interactive)
+  (set-window-width 81))
+
+(defun set-120-columns ()
+  "Set the selected window to 80 columns."
+  (interactive)
+  (set-window-width 120))
+
+(defun prev-window ()
+  (interactive)
+  (other-window -1))
+
 (global-set-key "\C-x\C-u" 'line-to-top)
 (global-set-key "\M-j" 'scroll-one-line-up)
 (global-set-key "\M-u" 'scroll-one-line-down)
@@ -130,7 +147,7 @@
 ;;; From this point onwards init.el contains Dinesh Weerapurage's changes
 ;;; http://ergoemacs.org/emacs/emacs_make_modern.html
 ;;; insert matching paranthesis and brackets works with emacs24
-;; (electric-pair-mode 1) 
+;;; (electric-pair-mode 1)
 
 (show-paren-mode 1)
 (setq show-parent-style 'expression)
@@ -139,11 +156,12 @@
 ;; Don't create backup files
 (setq make-backup-files nil) ; stop creating those backup~ files
 (setq auto-save-default nil) ; stop creating those #autosave# files
- 
 ;;;; nav ;;;;
 (require 'nav)
 ;;(nav-disable-overeager-window-splitting)
 ;;(require 'sr-speedbar)
+;;(setq speedbar-use-images nil)
+;;(speedbar 1)
 
 ;; yasnippet
 (require 'yasnippet)
@@ -160,10 +178,13 @@
 ;;; set the trigger key so that it can work together with yasnippet on tab key,
 ;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
 ;;; activate, otherwise, auto-complete will
+(setq ac-use-menu-map t)
 (ac-set-trigger-key "TAB")
 (ac-set-trigger-key "<tab>")
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
 
-;;; Markdown mode 
+;;; Markdown mode
 (autoload 'markdown-mode "markdown-mode"
         "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
@@ -171,11 +192,20 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+;; Adding python to path
+;; http://milkbox.net/note/installing-pymacs-rope-on-emacs-24/
+(push "/usr/local/bin" exec-path)
 ;;http://stackoverflow.com/questions/2855378/ropemacs-usage-tutorial
+(require 'python-mode)
+(setq py-load-pymacs-p 'nil)
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
-(require 'python-mode)
+
+(when (file-exists-p (expand-file-name "/Users/uweerd3/.emacs.d/lisp/Pymacs"))
+  (ac-ropemacs-initialize)
+  (ac-ropemacs-setup))
+
 (autoload 'pymacs-apply "pymacs")
 (autoload 'pymacs-call "pymacs")
 (autoload 'pymacs-eval "pymacs" nil t)
@@ -184,7 +214,7 @@
 (pymacs-load "ropemacs" "rope-")
 (setq ropemacs-enable-autoimport t)
 
-;;(require 'auto-complete)
+(require 'auto-complete)
 ;;(global-auto-complete-mode t)
 
 (add-hook 'python-mode-hook
@@ -203,3 +233,15 @@
 (defun flycheck-python-setup ()
   (flycheck-mode))
 (add-hook 'python-mode-hook 'flycheck-python-setup)
+
+;; making iterm emacs friendly
+(set-keyboard-coding-system nil)
+
+;;start emacs server-mode
+;; (server-start)
+(set-cursor-color "#adff2f")
+
+
+;;Clojur Mode:
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)

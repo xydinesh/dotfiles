2fd5758fe4785186e7c7fe203e09ac0ce8376fdf
Dinesh Weerapurage <xydinesh@gmail.com>
2020-01-05 18:43:05 -0700

    dev branch

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,7 +23,6 @@
 (require 'ansi-color)
 (require 'recentf)
 (require 'linum)
-(require 'smooth-scrolling)
 (require 'whitespace)
 (require 'dired-x)
 (require 'compile)
@@ -39,24 +38,6 @@
 (setq suggest-key-bindings t)
 (setq vc-follow-symlinks t)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
- '(column-marker-1 ((t (:background "red"))))
- '(diff-added ((t (:foreground "cyan"))))
- '(flymake-errline ((((class color) (background light)) (:background "Red"))))
- '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
- '(fundamental-mode-default ((t (:inherit default))))
- '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
- '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
- '(linum ((t (:foreground "black" :weight bold))))
- '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
- '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
- '(show-paren-match ((((class color) (background light)) (:background "black"))))
- '(vertical-border ((t nil))))
 
 ;; ------------
 ;; -- Macros --
@@ -148,20 +129,11 @@
 ;;; http://ergoemacs.org/emacs/emacs_make_modern.html
 ;;; insert matching paranthesis and brackets works with emacs24
 ;;; (electric-pair-mode 1)
-
 (show-paren-mode 1)
-(setq show-parent-style 'expression)
 (global-linum-mode 1)
-
 ;; Don't create backup files
 (setq make-backup-files nil) ; stop creating those backup~ files
 (setq auto-save-default nil) ; stop creating those #autosave# files
-;;;; nav ;;;;
-(require 'nav)
-;;(nav-disable-overeager-window-splitting)
-;;(require 'sr-speedbar)
-;;(setq speedbar-use-images nil)
-;;(speedbar 1)
 
 ;; yasnippet
 (require 'yasnippet)
@@ -171,96 +143,16 @@
         "~/.emacs.d/yasnippet/snippets"    ;; the default collection
         ))
 
-;;; using auto-complete
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-(ac-config-default)
-;;; set the trigger key so that it can work together with yasnippet on tab key,
-;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
-;;; activate, otherwise, auto-complete will
-(setq ac-use-menu-map t)
-(ac-set-trigger-key "TAB")
-(ac-set-trigger-key "<tab>")
-(define-key ac-menu-map "\C-n" 'ac-next)
-(define-key ac-menu-map "\C-p" 'ac-previous)
+;; Common-Lisp mode
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (paredit slime))))
 
-;;; Markdown mode
-(autoload 'markdown-mode "markdown-mode"
-        "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.txt\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-;; Adding python to path
-;; http://milkbox.net/note/installing-pymacs-rope-on-emacs-24/
-(push "/usr/local/bin" exec-path)
-;;http://stackoverflow.com/questions/2855378/ropemacs-usage-tutorial
-(require 'python-mode)
-(setq py-load-pymacs-p 'nil)
-(autoload 'python-mode "python-mode" "Python Mode." t)
-(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-
-(when (file-exists-p (expand-file-name "/Users/uweerd3/.emacs.d/lisp/Pymacs"))
-  (ac-ropemacs-initialize)
-  (ac-ropemacs-setup))
-
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-autoimport t)
-
-(require 'auto-complete)
-;;(global-auto-complete-mode t)
-
-(add-hook 'python-mode-hook
-          (lambda () (define-key python-mode-map (kbd "DEL") 'py-electric-backspace)))
-(normal-erase-is-backspace-mode 0)
-(setq python-check-command "pyflakes")
-(require 'tramp)
-(autoload 'python-pep8 "python-pep8");;
-(autoload 'pep8 "python-pep8")
-
-
-(load-theme 'misterioso)
-(require 'column-marker)
-(add-hook 'python-mode-hook (lambda () (interactive) (column-marker-1 80)))
-
-(defun flycheck-python-setup ()
-  (flycheck-mode))
-(add-hook 'python-mode-hook 'flycheck-python-setup)
-
-;; making iterm emacs friendly
-(set-keyboard-coding-system nil)
-
-;;start emacs server-mode
-;; (server-start)
-(set-cursor-color "#adff2f")
-
-
-;;Clojur Mode:
-(add-hook 'cider-repl-mode-hook #'company-mode)
-(add-hook 'cider-mode-hook #'company-mode)
-
-;; ispell
-(dolist (hook '(text-mode-hook))
-  (add-hook hook (lambda () (flyspell-mode 1))))
-
-(dolist (hook '(Markdown-mode-hook))
-  (add-hook hook (lambda () (flyspell-mode 1))))
-
-;;GO Mode.
-(with-eval-after-load 'go-mode
-   (require 'go-autocomplete))
-(setenv "GOPATH" "/Users/uweerd3/devs/gocode/")
-(add-to-list 'exec-path "/Users/uweerd3/devs/gocode/bin")
-(add-hook 'before-save-hook 'gofmt-before-save)
-(defun auto-complete-for-go ()
-  (auto-complete-mode 1))
-(add-hook 'go-mode-hook 'auto-complete-for-go)
-
-(setq ispell-program-name "/usr/local/bin/aspell")
+;; Set your lisp system and, optionally, some contribs
+(setq slime-contribs '(slime-fancy))
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+  ;; Replace "sbcl" with the path to your implementation
+(setq inferior-lisp-program "sbcl")
